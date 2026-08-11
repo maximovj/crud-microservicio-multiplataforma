d5f88eda79213f8d77a85f42d2a2d4268eb9cec7
Vìctor J. <maximovj@outlook.com>
2026-08-10 23:59:43 -0600

    * Se agrega un nuevo  microservicio para prestamos (loans)

---
+++ ms-api-loans/src/test/java/com/github/maximovj/SpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maximovj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
