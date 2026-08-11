bc4861ef7680b0bf2ca8416d8b7152a11aa31bf0
Vìctor J. <maximovj@outlook.com>
2026-08-11 00:34:13 -0600

    * Se modifica puerto de microservicio pagos (payments) y se agrega un enpoint

--- ms-api-payments/src/main/java/com/github/maximovj/ApiResource.java
+++ ms-api-payments/src/main/java/com/github/maximovj/ApiResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/")
+public class ApiResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String apiTest() {
+        return "Quarkus | API Payments v1.0";
     }
 }
