8af551e3bfefad55c568aa830ed3a21f27d8b1c3
Vìctor J. <maximovj@outlook.com>
2026-08-13 23:37:39 -0600

    * Se agrega crud para `users` y se agrega prefijo api/

--- ms-api-users/src/main.ts
+++ ms-api-users/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Sea agrega prefijo api/
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3005); // Se modifica el puerto
 }
 bootstrap();
