ea0c318528bc2870508e595caabe4563557ab082
Vìctor J. <maximovj@outlook.com>
2026-08-13 22:53:43 -0600

    * Se configura middleware morgan en modo desarrollador

--- ms-api-notifications/src/index.js
+++ ms-api-notifications/src/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const morgan = require('morgan');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 app.get('/',(req, res) => {
     res.send('API Notifications v1.0')
