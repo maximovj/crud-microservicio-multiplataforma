c1116a20f300cc96b863b0d24865a70076fc8455
Vìctor J. <maximovj@outlook.com>
2026-08-15 01:18:49 -0600

    * Se activa CORS

--- ms-api-users/src/main.ts
+++ ms-api-users/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Sea agrega prefijo api/
   app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3005); // Se modifica el puerto
 }
 bootstrap();
