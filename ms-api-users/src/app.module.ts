dedaee10cfd4d55089edd8cbb7562a9f65df220b
Vìctor J. <maximovj@outlook.com>
2026-08-15 01:04:26 -0600

    * Se configura conexión a mysql y typeorm

--- ms-api-users/src/app.module.ts
+++ ms-api-users/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'mysqluser',
+      password: 'mysqlpassword',
+      database: 'mysqldb',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
