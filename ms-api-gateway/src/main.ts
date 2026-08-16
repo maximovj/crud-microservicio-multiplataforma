483fd15a274f51b1ad4210e371bc6a7e6519045f
Vìctor J. <maximovj@outlook.com>
2026-08-16 00:28:08 -0600

    * ms-api-gateway:  Se  define variables de entorno para la api
    - Se crea example para definir variables de entorno

--- ms-api-gateway/src/main.ts
+++ ms-api-gateway/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const prefix = process.env.PREFIX_MS_API_GATEWAY || 'ms-api-gateway/api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(prefix);
   await app.listen(process.env.PORT ?? 3003); // Se modifica el puerto
 }
 bootstrap();
