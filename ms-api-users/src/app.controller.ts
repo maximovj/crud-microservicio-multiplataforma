eb6c57e93be88d19db88784b9754c04c9a5e80b5
Vìctor J. <maximovj@outlook.com>
2026-08-29 11:38:11 -0600

    ms-api-users : Se agrega documentación swagger para los controllers (controladores)

--- ms-api-users/src/app.controller.ts
+++ ms-api-users/src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('Health')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'API en funcionamiento',
+    description: 'Verifica que la API está funcionando correctamente',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
